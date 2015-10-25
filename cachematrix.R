e33d00f00ad248449bb73c6c87176a176ffbb1eb
daffodil2807 <daffodil2807@users.noreply.github.com>
2015-10-25 14:03:22 +0530

    Update cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a list using makeCacheMatrix and assign the following function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function()x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function()inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+##  This function is exclusively  valid only for inverse matrix.
+# If output is inverse it will give result.
+# Else, it computes the inverse, sets the value in the cache via
+# setinverse function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        inv <- x$getinverse()
+          if(!is.null(inv)) {
+       message("getting cached data.")
+               return(inv)
+          }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
+## Example for the assignment
+x = rbind(c(2,-4), c(-4, 2))## creation of matrix
+m = makeCacheMatrix(x) ## define variable for command
+m$get() ## run output command
+
+cacheSolve(m) ## this command will work only if inverse matrix
+
